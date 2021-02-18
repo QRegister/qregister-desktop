ff21f4f0119112cb3865a9351d133192be707466
Deniz Karakay <deniz@karakay.me>
2021-02-18 17:25:38 +0300

    first commit

---
+++ qr.py
@@ -0,0 +1,12 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('test')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('test.png')
