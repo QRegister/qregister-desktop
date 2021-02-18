218897b7448b5910bf0e5f37110e1977276b4d69
Deniz Karakay <deniz@karakay.me>
2021-02-18 19:04:26 +0300

    buttons updated

--- qr_gui.py
+++ qr_gui.py
@@ -5,19 +5,28 @@ import pyqrcode
 window = Tk()
 window.attributes('-fullscreen', True)
 window.title("Test")
-window.geometry("600x600")
+#window.geometry("600x600")
+window.grid_rowconfigure(2, weight=1)
+window.grid_columnconfigure(3, weight=1)
+
+
+def close():
+    window.destroy()
 
 
 def generate():
     qr = pyqrcode.create("test")
-    photo = BitmapImage(data=qr.xbm(scale=16))
+    photo = BitmapImage(data=qr.xbm(scale=32))
     qr_image.config(image=photo)
     qr_image.photo = photo
 
 
-button = Button(window, text="Show", command=generate)
-button.grid(row=0, column=0, padx=3, pady=3)
+button = Button(window, text="Show", width=10, height=5, command=generate)
+button.grid(row=0, column=0, padx=10, pady=10)
+
+button = Button(window, text="Exit", width=10, height=5, command=close)
+button.grid(row=0, column=4, padx=10, pady=10)
 
 qr_image = Label(window)
-qr_image.grid(row=4, column=1, padx=3, pady=3)
+qr_image.grid(row=2, column=3, padx=3, pady=3)
 window.mainloop()
