9dcb36146a87ad8d9231ccfe0fc4e6f8da2fd463
Deniz Karakay <deniz@karakay.me>
2021-06-23 11:13:36 +0300

    Barcode requirements added

---
+++ core/barcode_reader.py
@@ -0,0 +1,18 @@
+import time
+import cv2
+from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+camera = True
+while camera:
+    success, frame = cap.read()
+
+    for code in decode(frame):
+        print(code.type)
+        print(code.data.decode('utf-8'))
+        time.sleep(5)
+
+    cv2.imshow('Testing-code-scan', frame)
+    cv2.waitKey(1)
