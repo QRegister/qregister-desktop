09c8b5dc8d23ae0dc2ca0ccbb1f4edb15a5773f5
Deniz Karakay <deniz@karakay.me>
2021-06-23 12:25:14 +0300

    Barcode reader

--- core/barcode_reader.py
+++ core/barcode_reader.py
@@ -1,6 +1,6 @@
 import time
-import cv2
 from pyzbar.pyzbar import decode
+import cv2
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -12,7 +12,7 @@ while camera:
     for code in decode(frame):
         print(code.type)
         print(code.data.decode('utf-8'))
-        time.sleep(5)
+	time.sleep(5)
 
     cv2.imshow('Testing-code-scan', frame)
     cv2.waitKey(1)
