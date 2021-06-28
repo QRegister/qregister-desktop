1f72cbfababc2135ebf8ee4f5cdd3b7caefe16cd
Deniz Karakay <deniz@karakay.me>
2021-06-28 18:50:48 +0300

    Barcode Used Codes

--- core/barcode_reader.py
+++ core/barcode_reader.py
@@ -5,14 +5,21 @@ import cv2
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+used_codes = []
+
 camera = True
 while camera:
     success, frame = cap.read()
 
     for code in decode(frame):
-        print(code.type)
-        print(code.data.decode('utf-8'))
-        time.sleep(5)
+        barcode_str = code.data.decode('utf-8')
+        if barcode_str not in used_codes:
+            print(code.type)
+            print(barcode_str)
+            used_codes.append(barcode_str)
+            time.sleep(5)
+        elif barcode_str in used_codes:
+            pass
 
-    cv2.imshow('Testing-code-scan', frame)
+    cv2.imshow('QRegister Barcode Scanner', frame)
     cv2.waitKey(1)
