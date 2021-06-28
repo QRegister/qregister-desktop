7a4dafecbf387578521bb3b4eef76b928e7e6504
Deniz Karakay <deniz@karakay.me>
2021-06-28 18:32:10 +0300

    Barcode reader update

--- core/barcode_reader.py
+++ core/barcode_reader.py
@@ -12,7 +12,7 @@ while camera:
     for code in decode(frame):
         print(code.type)
         print(code.data.decode('utf-8'))
-	time.sleep(5)
+        time.sleep(5)
 
     cv2.imshow('Testing-code-scan', frame)
     cv2.waitKey(1)
